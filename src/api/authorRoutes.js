01c73b2a827899a3832197a0a5e8b532880b7795
Marius <marius@example.com>
2022-04-19 10:58:22 +0300

    empty authors routes

--- src/api/authorRoutes.js
+++ src/api/authorRoutes.js
@@ -5,8 +5,14 @@ const authorRoutes = express.Router();
 
 // routes
 // POST /api/author - sukuria nauja autoriu
+authorRoutes.post('/author', async (req, res) => {
+  res.json('creating an author');
+});
 
 // GET /api/author - gauti visus autorius
+authorRoutes.get('/author', async (req, res) => {
+  res.json('authors index');
+});
 
 // GET /api/author/:authorId - gauti konkretu autoriu
 
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const authorRoutes = require('./api/authorRoutes');
 const booksRoutes = require('./api/booksRoutes');
 const userRoutes = require('./api/usersRoutes');
 const { PORT } = require('./config');
@@ -15,5 +16,6 @@ app.get('/', (req, res) => res.json('OK'));
 // Routes
 app.use('/api', userRoutes);
 app.use('/api', booksRoutes);
+app.use('/api', authorRoutes);
 
 app.listen(PORT, () => console.log('server online, PORT', PORT));
